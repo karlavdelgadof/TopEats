f7ea30a38ec0c7adac5036494a076e3dc21b9460
karlavdelgadof <karla.delgado@toptal.com>
2023-01-06 10:45:18 -0500

    Add tests for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should define_enum_for(:status).
+    with_values(pending: 0, accepted: 1, preparing: 2, delivered: 3)}
+    it { should validate_numericality_of(:total_price).is_greater_than_or_equal_to(0)}
+  end
+
+  describe 'associations' do
+    it { should belong_to(:courier)}
+    it { should belong_to(:customer)}
+    it { should have_many(:menu_items)}
+  end
 end
