e9e4af43e2209337e69df3166497273a30beb3ce
karlavdelgadof <karla.delgado@toptal.com>
2023-01-06 10:52:06 -0500

    Update MenuItem model w validations/associations

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,7 @@
 class MenuItem < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates_presence_of :name
+  validates_length_of :name, maximum: 250
 end
