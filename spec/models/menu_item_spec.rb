1146f20567b51bd6c08dbf310c2f318195f8bc04
karlavdelgadof <karla.delgado@toptal.com>
2023-01-06 10:51:44 -0500

    Add tests for MenuItem model

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_length_of(:name).is_at_most(250)}
+  end
+
+  describe 'associations' do
+    it { should have_many(:orders)}
+  end
 end
