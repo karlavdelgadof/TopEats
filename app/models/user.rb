78336c56a4ca8b1beceef6e211d86c614c4289a8
karlavdelgadof <karla.delgado@toptal.com>
2023-01-06 09:18:45 -0500

    Update user model with validations/associations

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  belongs_to :address
+  has_many :vehicles, foreign_key: 'courier_id', dependent: :destroy
+  has_many :orders, foreign_key: 'courier_id', dependent: :destroy
+  has_many :orders, foreign_key: 'customer_id', dependent: :destroy
+
+  validates_presence_of :full_name, :type
+  validates_length_of :full_name, maximum: 250, on: :create
+  validates_format_of :phone_number, with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+  message: "- Phone numbers must be in xxx-xxx-xxxx format."
+end
