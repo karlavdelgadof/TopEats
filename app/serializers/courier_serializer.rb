b5e045c63c5ff638f1df90ec10ae344c9a0c5058
karlavdelgadof <karla.delgado@toptal.com>
2023-01-11 08:33:22 -0500

    Generate serializers for Users children models

--- app/serializers/courier_serializer.rb
+++ app/serializers/courier_serializer.rb
@@ -2,4 +2,5 @@ class CourierSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :email, :phone_number
   belongs_to :address
   has_many :vehicles
+  has_many :orders
 end
--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :email, :phone_number
   belongs_to :address
+  has_many :orders
 end
