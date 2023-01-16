8b14f48ade7dcc82aa62b907dc9d39fc24726850
karlavdelgadof <karla.delgado@toptal.com>
2023-01-15 20:49:26 -0500

    Add CRUD actions to address controller

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -3,15 +3,39 @@ class AddressController < ApplicationController
     render json: Address.all
   end
 
-  def show
-  end
+  def show;end
 
   def create
+    address = Address.new(address_params)
+    if address.save
+      render json: address, status: :ok
+    else
+      render json: address.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    address = find_address
+    if address.update(address_params)
+      render json: address , status: :found
+    else
+      render json: address.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    address = find_address
+    address.destroy
+    render status: :not_found
+  end
+
+  private
+
+  def find_address
+    Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:description)
   end
 end
