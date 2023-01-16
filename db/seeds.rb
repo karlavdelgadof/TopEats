f68bd0aa8d4e9cef1bd38e4f2a998c097909f46d
karlavdelgadof <karla.delgado@toptal.com>
2023-01-15 20:46:14 -0500

    Generate seeds for app data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+20.times do |i|
+  Address.create!(description: "Address #{i}")
+end
+
+10.times do |i|
+  Customer.create!(full_name: "Person #{i}", email: "email.person#{i}@example.com", phone_number: "(143)-977-2346", address_id: rand(1..20))
+end
+
+10.times do |i|
+  Courier.create!(full_name: "Person #{i}", email: "email.person#{i}@example.com", phone_number: "(243)-987-2347", address_id: rand(1..20))
+end
+
+10.times do
+  Vehicle.create!(courier_id: rand(1..10), vehicle_type: rand(0..2))
+end
+
+20.times do
+  Order.create!(status: rand(0..3), courier_id: rand(11..20), customer_id: rand(1..10), total_price: rand(5..9999))
+end
+
+
+10.times do |i|
+  MenuItem.create!(name: "Item #{i}")
+end
+
+30.times do
+  OrderItem.create!(order_id: rand(1..20), menu_item_id: rand(1..10))
+end
+
